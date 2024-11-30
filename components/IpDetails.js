290322a9f0f57c674e39ad2614b40ec55668bb15
Aleeza Rubab <malikaleeza886@gmail.com>
2024-11-30 13:15:35 -0800

    first commit

--- components/IpDetails.js
+++ components/IpDetails.js
@@ -1,8 +1,9 @@
-'use client'; 
+'use client'; // Add this directive to enable client-side rendering
 
 import useIpDetails from '../hooks/useIpDetails';
 import useBrowserDetails from '../hooks/useBrowserDetails';
 import Loader from './Loader';
+import Image from 'next/image';
 
 const IpDetails = () => {
   const { loading, error, data: ipData } = useIpDetails();
@@ -27,67 +28,65 @@ const IpDetails = () => {
         </p>
       </div>
 
-      {/* Scrollable Table Container */}
-      <div className="overflow-x-auto">
-        <table className="w-full bg-zinc-50 rounded-lg shadow-lg min-w-[600px]">
-          <tbody>
-            {/* IP Row */}
-            <tr className="border-t-2 border-b-2 border-green-600">
-              <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2">IP</th>
-              <td className="p-4 text-lg text-left break-all">{ipData.ip}</td>
-            </tr>
+      {/* Table */}
+      <table className="w-full bg-zinc-50 rounded-lg shadow-lg">
+        <tbody>
+          {/* IP Row */}
+          <tr className="border-b-2 border-green-600">
+            <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2 ">IP</th>
+            <td className="p-4 text-lg text-left">{ipData.ip}</td>
+          </tr>
 
-            {/* City Row */}
-            <tr className="border-t-2 border-b-2 border-green-600">
-              <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2">City</th>
-              <td className="p-4 text-lg text-left">{ipData.city || 'N/A'}</td>
-            </tr>
+          {/* City Row */}
+          <tr className="border-t-2 border-b-2 border-green-600 ">
+            <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2 ">City</th>
+            <td className="p-4 text-lg text-left">{ipData.city}</td>
+          </tr>
 
-            {/* Capital Row */}
-            <tr className="border-t-2 border-b-2 border-green-600">
-              <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2">Capital</th>
-              <td className="p-4 text-lg text-left">{ipData?.region}</td>
-            </tr>
+          {/* Region Row */}
+          <tr className="border-t-2 border-b-2 border-green-600">
+            <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2 ">Capital</th>
+            <td className="p-4 text-lg text-left">{ipData.region}</td>
+          </tr>
 
-            {/* Country Row */}
-            <tr className="border-t-2 border-b-2 border-green-600">
-              <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2">Country</th>
-              <td className="p-4 text-lg text-left">{ipData.country}</td>
-            </tr>
+          {/* Country Row */}
+          <tr className="border-t-2 border-b-2 border-green-600">
+            <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2 ">Country</th>
+            <td className="p-4 text-lg text-left">{ipData.country}</td>
+          </tr>
 
-            {/* Location Row */}
-            <tr className="border-t-2 border-b-2 border-green-600">
-              <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2">Postal</th>
-              <td className="p-4 text-lg text-left">{`${ipData.postal}`}</td>
-            </tr>
+          {/* Location Row */}
+          <tr className="border-t-2 border-b-2 border-green-600">
+            <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2 ">Postal</th>
+            <td className="p-4 text-lg text-left">{ipData.postal}</td>
+          </tr>
 
-            {/* Browser Row */}
-            <tr className="border-t-2 border-b-2 border-green-600">
-              <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2">Browser</th>
-              <td className="p-4 text-lg text-left">{browser}</td>
-            </tr>
+          {/* Browser Row */}
+          <tr className="border-t-2 border-b-2 border-green-600">
+            <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2 ">Browser</th>
+            <td className="p-4 text-lg text-left">{browser}</td>
+          </tr>
 
-            {/* Screen Row */}
-            <tr className="border-t-2 border-b-2 border-green-600">
-              <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2">Screen</th>
-              <td className="p-4 text-lg text-left">{screenSize}</td>
-            </tr>
+          {/* Screen Row */}
+          <tr className="border-t-2 border-b-2 border-green-600">
+            <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2 ">Screen</th>
+            <td className="p-4 text-lg text-left">{screenSize}</td>
+          </tr>
 
-            {/* Cookies Enabled Row */}
-            <tr className="border-t-2 border-b-2 border-green-600">
-              <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2">Cookies Enabled</th>
-              <td className="p-4 text-lg text-left">{cookiesEnabled.toString()}</td>
-            </tr>
+          {/* Cookies Enabled Row */}
+          <tr className="border-t-2 border-b-2 border-green-600">
+            <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2 ">Cookies Enabled</th>
+            <td className="p-4 text-lg text-left">{cookiesEnabled.toString()}</td>
+          </tr>
 
-            {/* Continent Row */}
-            <tr className="border-t-2 border-b-2 ">
-              <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2">Timezone</th>
-              <td className="p-4 text-lg text-left">{ipData.timezone}</td>
-            </tr>
+          {/* Org Row */}
+          <tr className="border-t-2 border-b-2 ">
+            <th className="p-4 text-center font-bold text-white bg-green-600 uppercase border-r-2 border-white border-b-2 ">Timezone</th>
+            <td className="p-4 text-lg text-left">{ipData.timezone}</td>
+          </tr>
 
-          </tbody>
-        </table>
-      </div>
+        </tbody>
+      </table>
     </div>
   );
 };
