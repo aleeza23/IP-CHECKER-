2899101001642f95ea903fa72d166636c5f67ddc
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-12-18 18:56:03 +0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -45,7 +45,7 @@ const Footer = () => {
                 </div>
                 <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
                     <p className="text-sm text-gray-100">
-                        © 2024 IP Checker Powered by <Link className="underline" href={'https://thestockit.net/'}>The Stockit</Link>. All rights reserved!
+                        © 2024 IP Checker Developed by <Link className="underline" href={'https://www.linkedin.com/in/aleezarubab'}>Aleeza R.</Link> All rights reserved!
                     </p>
                     <div className="flex items-center mt-4 space-x-4 sm:mt-0">
                         <Link
